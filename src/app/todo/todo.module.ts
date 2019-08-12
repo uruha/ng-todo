6f96404fa85dba27b70713d499bfea774f1afe8a
uruha <kochmusica@gmail.com>
2019-08-12 14:09:52 +0900

    add httpclient and change provider

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { TodoRoutingModule } from './todo-routing.module';
 import { TodoComponent } from './todo.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TodoService } from './todo.service';
 
 @NgModule({
   declarations: [TodoComponent],
-  imports: [CommonModule, ReactiveFormsModule, TodoRoutingModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    TodoRoutingModule
+  ],
+  providers: [TodoService]
 })
 export class TodoModule {}
