f20eca977dd6952630443b8780dc81db1438d73a
uruha <kochmusica@gmail.com>
2019-08-12 13:00:22 +0900

    separate todo module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoComponent } from './todo/todo.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoComponent],
-  imports: [BrowserModule, ReactiveFormsModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
